4be8cc70a5d7befa4ad504c529e3ef48a15d7490
Rabbarbaro <barbi05.kn@gmail.com>
2025-05-05 19:37:04 +0200

    Corrected mistakes in Test08

--- Test/Test 08/es2.m
+++ Test/Test 08/es2.m
@@ -8,11 +8,10 @@ a = -1;
 b = 1;
 
 % dalla teoria la stima a priori dell'errore di interpolazione con lagrange su n+1 nodi equispaziati è:
-%   - H = (b-a) / n
-%   - E_H <= H^2 / 8 * max|f''(x)|, x in [a,b]
+%   e_n = 1/(4 * (n+1)) * ((b-a)/n)^(n+1) * max|d(n+1)f(x)|
 
-d2f = @(x) -pi^2 * cos(pi * x); % derivata seconda di f
-max_d2f = max(abs(d2f(linspace(a, b, 100)))); % massimo della derivata seconda di f in [a,b] (è pi^2)
-H = (b - a) / 2; % passo di interpolazione
-E_H = H^2 / 8 * max_d2f; % errore di interpolazione
-disp(['Errore di interpolazione: ', num2str(E_H)]);
+% la derivata di f n-esima è pi^n * funzione trigonometrica limitata a -1/1
+
+% quindi:
+%   e_n = 1/(4 * (n+1)) * (2/n)^(n+1) * pi^n+1
+%   e_n = 2^(n-1) / (n+1) * (pi/n)^(n+1)
--- Test/Test 08/es7.m
+++ Test/Test 08/es7.m
@@ -6,10 +6,13 @@ M1 = 10;
 err_interp_max1 = 1e-2;
 err_interp_max2 = 1e-5;
 
+% H1 = (b-a) / M1
+
 % max|f(x) - s3(x)| <= C * H^4 * max|f''''(x)|
 
-% err_interp_max1 = C_dot_max_d4f * 1/M1
-C_dot_max_d4f = err_interp_max1 * M1;
+% err_interp_max1 = C_dot_max_d4f_dot_b-a4 * 1/M1^4
+Const = err_interp_max1 * M1^4;
 
-% err_interp_max1 = C_dot_max_d4f * 1/M2
-M2 = C_dot_max_d4f / err_interp_max2
+% err_interp_max1 = C_dot_max_d4f_dot_b-a4 * 1/M2^4
+M2 = (Const / err_interp_max2)^(1/4)
+M2 = ceil(M2)
